1ba0626ce3574d4182e4138802edd76e5f6e4cf7
Ulrik <ulrik.strid@outlook.com>
2021-01-20 15:52:23 +0100

    Add nix setup

---
+++ nix/generic.nix
@@ -0,0 +1,38 @@
+{ pkgs, stdenv, lib, ocamlPackages, static ? false, doCheck }:
+
+with ocamlPackages;
+
+rec {
+  reenv = buildDunePackage {
+    pname = "reenv";
+    version = "0.3.1-dev";
+
+    src = lib.filterGitSource {
+      src = ./..;
+      dirs = [ "lib" "executable" "test" ];
+      files = [ "dune-project" "oidc.opam" ];
+    };
+
+    useDune2 = true;
+
+    propagatedBuildInputs = [
+      cmdliner
+      luv
+      logs
+      fmt
+    ];
+
+    buildInputs = [
+      alcotest
+      junit
+      junit_alcotest
+    ];
+
+    inherit doCheck;
+
+    meta = {
+      description = "dotenv-cli written in reason";
+      license = stdenv.lib.licenses.bsd3;
+    };
+  };
+}
